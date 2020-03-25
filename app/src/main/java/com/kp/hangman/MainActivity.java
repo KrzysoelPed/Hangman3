a453855d5d1fb50d5d4acbabfd073800bcc3ad75
Krzysztof Peda <krzysztof@retia.pl>
2020-03-25 17:18:56 +0100

    Update MainActivity.java

--- app/src/main/java/com/kp/hangman/MainActivity.java
+++ app/src/main/java/com/kp/hangman/MainActivity.java
@@ -8,22 +8,28 @@ import com.google.android.material.snackbar.Snackbar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.text.InputType;
 import android.view.Gravity;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
 import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import static java.lang.Thread.sleep;
 
 public class MainActivity extends AppCompatActivity {
 
     public String mystery_word;
     public String guess_word;
+    public String guess_char;
     int gallows_state = 0;
     int index;
     String[] words;
-
+    String displaying_word="";
 
 
     @Override
@@ -31,10 +37,15 @@ public class MainActivity extends AppCompatActivity {
         words = getResources().getStringArray(R.array.words);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+
+        StartNewGame();
+
         FloatingActionButton fab = findViewById(R.id.fab);
+        //fab.setImageResource(R.drawable.ic_launcher_background);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -44,8 +55,7 @@ public class MainActivity extends AppCompatActivity {
                             @Override
                             public void onClick (View view)
                             {
-                                int number = (int)(words.length * Math.random());
-                                mystery_word = words[number];
+                                StartNewGame();
                             }
                         })
                         .show();
@@ -86,33 +96,45 @@ public class MainActivity extends AppCompatActivity {
             case R.id.button2:
                 WordCheck();
                 break;
+            case R.id.button3:
+                WordMessage();
+
+                StartNewGame();
+                break;
             default:
         }
     }
 
+    public void StartNewGame()
+    {
+        int number = (int)(words.length * Math.random());
+        mystery_word = words[number];
+
+        gallows_state = 0;
+        HangmanImage();
+        DisplayMysteryWordAsStars(mystery_word);
+    }
     public void LetterCheck()
     {
         EditText txt = (EditText)findViewById(R.id.editext);
         guess_word = txt.getText().toString();
-
-        char[] guess_word_array = guess_word.toCharArray();
-        char guess_char = guess_word_array[0]; // jezeli wpiszemy wiecej niż jedną literę, to klikając sprawdź literę, sprawdzimy wystąpienia pierwszej z wporwadzonych liter
+        guess_char = guess_word.substring(0,1);
 
         if(!guess_word.isEmpty())
         {
-            if(mystery_word.contains(guess_word))
+            if(mystery_word.contains(guess_char))
             {
+                //m_word.setInputType(InputType.TYPE_CLASS_TEXT); //programistyczny sposób na przełączanie się miedyz widokiem hasła, a zwyklym tekstem w obiekcie typu textview
 
-                index = mystery_word.indexOf(guess_word,0);
-                StringBuilder temp = new StringBuilder(guess_word);
-                temp.setCharAt(index,guess_char);
+                DisplayMysteryWordsWithSomeLetters(guess_char,mystery_word,displaying_word);
+                ClearEditText();
 
-                // tu zostało zrobić warunek że zgadlismy wszystkie literki
             }
             else
             {
                 gallows_state = gallows_state + 1;
-                NegativeMessage();
+                // NegativeMessage();
+                ClearEditText();
                 HangmanImage();
             }
         }
@@ -131,18 +153,33 @@ public class MainActivity extends AppCompatActivity {
 
         Boolean game_check =  mystery_word.equals(guess_word);
 
-        if(game_check == true)
+        if(!guess_word.isEmpty())
         {
-            PositiveMessage();
-            NewGameMessage();
+            if (game_check == true)
+            {
+                PositiveMessage();
+                ClearEditText();
+                // NewGameMessage();
+
+            }
+            else
+            {
+                gallows_state = gallows_state + 1;
+
+                // NegativeMessage();
+                ClearEditText();
+                HangmanImage();
+            }
         }
         else
         {
-            gallows_state = gallows_state + 1;
-
-            NegativeMessage();
-            HangmanImage();
+            ErrorMessage();
         }
+    }
+    public void DisplayWord()
+    {
+        TextView m_word = (TextView) findViewById(R.id.textView);
+        m_word.setText(mystery_word);
     }
     public void HangmanImage()
     {
@@ -188,6 +225,7 @@ public class MainActivity extends AppCompatActivity {
                 gallows.setImageResource(R.drawable.hangman10);
                 break;
             default:
+                NewGameMessage();
         }
     }
 
@@ -200,8 +238,9 @@ public class MainActivity extends AppCompatActivity {
 
     public void PositiveMessage()
     {
-        Toast toast = Toast.makeText(getApplicationContext(),"Gratulacje, odgadłeś słowo !",Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(),"Gratulacje, odgadłeś słowo ! \n \n Aby rozpocząć nową grę, kliknij klawisz ze znakiem zapytania :)",Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER,0,0);
+
         toast.show();
     }
 
@@ -213,9 +252,40 @@ public class MainActivity extends AppCompatActivity {
     }
     public void NewGameMessage()
     {
-        Toast toast = Toast.makeText(getApplicationContext(),"Aby rozpocząć nową grę, kliknij klawisz ze znakiem zapytania :)",Toast.LENGTH_SHORT);
+        Toast toast = Toast.makeText(getApplicationContext(),"Aby rozpocząć nową grę, kliknij klawisz 'Play' :)",Toast.LENGTH_LONG);
         toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER,0,0);
         toast.show();
+    }
+    public void WordMessage()
+    {
+        Toast toast = Toast.makeText(getApplicationContext(),"Było to słowo: " + mystery_word,Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER_VERTICAL|Gravity.CENTER,0,0);
+        toast.show();
+    }
+    public void ClearEditText()
+    {
+        EditText txt = (EditText)findViewById(R.id.editext);
+        txt.getText().clear();
+    }
+
+    public void DisplayMysteryWordAsStars(String mystery_word)
+    {
+        displaying_word="";
+        for(int i = 0; i < mystery_word.length(); i++)
+        {
+            displaying_word = displaying_word +"*";
+        }
+        TextView m_word = (TextView) findViewById(R.id.textView);
+        m_word.setText(displaying_word);
+    }
+
+    public void DisplayMysteryWordsWithSomeLetters(String guess_word, String mystery_word, String displaing_word)
+    {
+        for (int i = -1; (i = mystery_word.indexOf(guess_word, i + 1)) != -1; i++) {
+            displaying_word = displaing_word.substring(0,i)+ guess_word + displaing_word.substring(i+1);
+        }
+        TextView m_word = (TextView) findViewById(R.id.textView);
+        m_word.setText(displaying_word);
     }
 
 
